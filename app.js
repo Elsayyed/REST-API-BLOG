a8c3e0b8437d15aceec0190e8dc3555c26edb06b
unknown <upmohammad@gmail.com>
2024-07-02 16:52:01 -0500

    Added signup route and laied the foundation for controllers/routers

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 dotenv.config();
 
 const connectMongoDb = require("./init/mongodb")
-
+const {authRoute} = require("./routes")
+const {errorHandler} = require("./middlewares")
 //Connect MongoDB
 connectMongoDb();
 // init app
@@ -13,7 +15,12 @@ const app = express();
 // 3rd party middlewares
 app.use(express.json({limit: "500mb"}));
 app.use(bodyParser.urlencoded({limit: "500mb", extended: true}));
+app.use(morgan("dev"));
 
+//route section
+app.use("/api/v1/auth", authRoute)
 
+//error handler section
+app.use(errorHandler)
 
 module.exports = app;
---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true, trim: true},
+    password: {type: String, required: true, minlength: 6},
+    // role --> 1 (super admin), role --> 2 (admin), role --> 3 (normal user) 
+    role: {type: Number, default: 3},
+}, {timestamps: true})
+
+const User = mongoose.model("user", userSchema)
+
+module.exports = User
